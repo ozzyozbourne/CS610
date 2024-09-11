b8a33a49e5d7a6613bb745527d5f70ed3ac5b1c7
ozzyozbourne <khanosaid726@gmail.com>
2024-09-11 00:47:19 -0400

    week1 completed

--- o_khan_p1a.c
+++ o_khan_p1a.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
-main(){
-    int sum = 0;
-    int i;
-    for(i = 1; i <= 100; i++) sum = sum + i;
-    cout << "the sum is -> " << sum << ENDL;
+ 
+int normal_summation(int);
+int gauss_summation(int);
+
+main(void){
+    /* const int res1 = normal_summation(100); */ 
+    const int res2 = gauss_summation(100); 
+    /* cout << "The sum upto 100 using normal method is -> " << res1 << ENDL; */
+    cout << "The sum upto 100 using gauss method is -> " << res2 << ENDL;
 }
+
+int normal_summation(const int number){
+    int sum = 0, iter = 1;
+    for(; iter <= number; iter++) sum = sum + iter;
+    return sum;
+}
+
+int gauss_summation(const int number){
+    return ((number + 1) * number ) / 2;
+}
+
